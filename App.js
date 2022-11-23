1a265f0d6afb659c1683d73fdac5cb1706c0e509
rmwhib <richmw1980@gmail.com>
2022-11-23 16:45:12 +0000

    added map and autocomplete

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React, { createContext, useContext, useState } from 'react'
 import { StyleSheet, Button, Text, View, TextInput } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  CurrentRenderContext,
+  NavigationContainer,
+} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { useNavigation } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+//mapdirections
+import MapViewDirections from 'react-native-maps-directions'
+const origin = { latitude: 51.67599, longitude: -1.7873 }
+const destination = { latitude: 51.7075, longitude: -1.7851 }
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDT52TGfcNkMdwaVhrgKjuixYKy3O2ftQ0'
+
+//findmaplocation
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+
+import MapView from 'react-native-maps'
 
 const Tab = createBottomTabNavigator()
 
@@ -14,6 +28,10 @@ export const AuthContext = createContext({
   setpassWord: () => {},
   wrongPassword: false,
   setwrongPassword: () => {},
+  originLat: '',
+  setOriginLat: () => {},
+  originLng: '',
+  setOriginLng: () => {},
 })
 
 const Stack = createStackNavigator()
@@ -25,10 +43,9 @@ const SignUpScreen = () => {
 
   return (
     <View style={styles.layout}>
-      <Text> {userName} </Text>
-      <Text> {passWord} </Text>
-
+      <Text> Enter UserName </Text>
       <TextInput onChangeText={setName} style={styles.input} />
+      <Text> Enter Password </Text>
       <TextInput onChangeText={setpassWord} style={styles.input} />
       <Button
         title="SignUp"
@@ -125,21 +142,18 @@ const MainNavigator = () => {
 const SignIn = () => {
   // use the variable
   return (
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {/* <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
       <Stack.Screen name="SignInScreen" component={SignInScreen} />
-      <Stack.Screen name="Tryagain" component={Tryagain} />
+      <Stack.Screen name="Tryagain" component={Tryagain} /> */}
       <Stack.Screen name="MainNavigator" component={MainNavigator} />
     </Stack.Navigator>
   )
 }
-
-// --- Onboarding screens ---
-const HomeScreen = () => (
-  <View style={styles.layout}>
-    <Text>mainscreen</Text>
-  </View>
-)
 
 const AccountScreen = () => (
   <View style={styles.layout}>
@@ -172,9 +186,63 @@ const App = () => {
   )
 }
 
+// --- Onboarding screens ---
+const HomeScreen = () => (
+  <View style={styles.mapContainer}>
+    <GooglePlacesAutocomplete
+      styles={{
+        textInput: { flex: 0, width: 400 },
+      }}
+      placeholder="Search"
+      query={{
+        key: GOOGLE_MAPS_APIKEY,
+        language: 'en', // language of the results
+      }}
+      fetchDetails={true}
+      onPress={(data, details) => console.log(details.geometry.location)}
+      onFail={(error) => console.error('fucked it')}
+      requestUrl={{
+        url:
+          'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
+        useOnPlatform: 'web',
+      }} // this in only required for use on the web. See https://git.io/JflFv more for details.
+    />
+
+    <Text style={styles.text}>mainscreen</Text>
+
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }}
+    >
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+      />
+    </MapView>
+  </View>
+)
+
 export default App
 
 const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  text: {
+    flex: 1,
+  },
+  map: {
+    flex: 2,
+    width: 400,
+    height: 400,
+  },
   layout: {
     flex: 1,
     justifyContent: 'center',
@@ -188,7 +256,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   input: {
-    height: 20,
+    height: 50,
+    width: 250,
     margin: 6,
     borderWidth: 1,
     padding: 5,
